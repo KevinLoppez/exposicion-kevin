027ddba3d0476f32ef7aa7ffabb38530e133b0e8
Kevin Lopez <kslopez709@misena.edu.co>
2022-11-22 12:04:03 -0500

    v3

---
+++ 54/Invoice.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+/*class Invoice
+{
+    public function __construct (public Customer $customer)
+    {
+    }
+    public function process(float $amount):void
+    {
+        echo 'Processing $' . $amount . 'invoice - ';
+    }
+}
